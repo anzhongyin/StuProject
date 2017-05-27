fb992b7c6bd7bb28f2e9e387bf80a23467497e47
anzhongyin <ar_2002@sina.com>
2017-05-27 10:04:26 +0800

    Update urls.py

--- StuProject/urls.py
+++ StuProject/urls.py
@@ -17,6 +17,5 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^index/', index),# add code
+    url(r'^admin/', admin.site.urls),    
 ]
